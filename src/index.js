aba7f108dbdbb3b1ac7329339a89eae3b08bd551
FalkZ <24669719+FalkZ@users.noreply.github.com>
2018-03-05 17:55:26 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
-import "./Icons.styl";
-import React from "react";
-import ReactDOM from "react-dom";
+import './Icons.styl'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-const Icons = () =>
-  ["Facebook", "Window"].map((icon, index) => (
-    <div className={"Icon " + icon} key={index} />
-  ));
+const getRandomNumber = range => Math.floor(Math.random() * Math.floor(range))
 
-ReactDOM.render(<Icons />, document.getElementById("Body"));
+const Random = arr => {
+	let randomArr = []
+	arr.map(value => {
+		let ran
+		for (
+			ran = getRandomNumber(arr.length);
+			typeof randomArr[ran] !== 'undefined';
+			ran = getRandomNumber(arr.length)
+		) {}
+
+		randomArr[ran] = value
+	})
+
+	return randomArr
+}
+
+const Icons = ({ icons }) =>
+	icons.map((icon, index) => (
+		<div id={index} className={'Icon ' + icon} key={index} />
+	))
+
+class Showcase extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { icons: props.icons }
+
+		this.reload = this.reload.bind(this)
+	}
+	reload() {
+		this.setState({ icons: Random(this.props.icons) })
+	}
+
+	render() {
+		return (
+			<div>
+				<Icons icons={this.state.icons} />
+				<button className="reload" onClick={this.reload}>
+					reload
+				</button>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+	<Showcase
+		icons={[
+			'Cross',
+			'Window',
+			'Plus',
+			'Minus',
+			'Facebook',
+			'Instagram',
+			'Youtube',
+			'Link',
+			'Menu',
+			'Home'
+		]}
+	/>,
+	document.getElementById('Body')
+)
